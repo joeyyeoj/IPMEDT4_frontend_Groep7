703d02890b4c85570a59d44941a3b8779d57d11f
MarZwa <mgm.zwaan@gmail.com>
2021-06-09 16:12:26 +0200

    Question box

---
+++ src/Questions.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Questions.css';
+
+const Questions = props => {
+    return (
+        <article className="test">
+            <section className="question">
+                {/* <p>Test</p> */}
+            </section>
+        </article>
+    );
+}
+
+export default Questions;
---
+++ src/questionOpen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const QuestionOpen = props => {
+    return (
+        <section>
+            <p>Test</p>
+        </section>
+    );
+}
+
+export default QuestionOpen;
