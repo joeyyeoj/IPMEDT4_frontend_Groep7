564458cd6428b97f073ff9e4418f96e86721ae70
Tijn <tijnvanderweijden@gmail.com>
2021-06-20 12:01:41 +0200

    get functie voor emails gefixt

--- src/EmailCard.js
+++ src/EmailCard.js
@@ -1,62 +1,70 @@
 import React from 'react';
 
 import './EmailCard.css';
-
 import SearchBar from './SearchBar.js';
+import FileUploadComponent from './test.js';
 
 import axios from "axios";
 
 class EmailCard extends React.Component {
 
-    state = { email: "", id: "" };
+    state = { emails: [], id: "" };
 
     componentDidMount() {
         this.getEmails(1);
         
     }
 
-    getEmails(num) {
-        let array = [];
-
-        const BASE_URL = "http://localhost:8000/api/mailgroep/"+ num +"/emailadressen/";
+    getEmails(emailgroepId) {
+        let emailArray = [];
+        const BASE_URL = "http://localhost:8000/api/mailgroep/"+ emailgroepId +"/emailadressen/";
         axios.get(BASE_URL).then(res => {
+            for(let i=0; i<res.data.length; i++) {
+                emailArray.push(res.data[i]['email']);
+                console.log(res.data[i].email);
+            }         
             this.setState({
-                name: this.res.name                
+                email: emailArray 
             });        
-            console.log(res.data.name);
+            
         });  
     }
 
-    onSubmit = (emailInput) => {
-        let bodyFormData = new FormData();
-        bodyFormData.append('email', emailInput);
-        bodyFormData.append('mailgroep-id', 1);
-        const BASE_URL = "http://localhost:8000/api/mailgroep/1/";
+    // onSubmit = (emailInput) => {
+    //     let bodyFormData = new FormData();
+    //     bodyFormData.append('email', emailInput);
+    //     bodyFormData.append('mailgroep-id', 1);
+    //     const BASE_URL = "http://localhost:8000/api/mailgroep/1/";
 
-        axios({
-            method: "post",
-            url: BASE_URL,
-            data: bodyFormData,
-            headers: { "Content-Type": "multipart/form-data" },
-        })
-        // kan later weg
-          .then(function (response) {
-            console.log(response);
-          })
-          .catch(function (response) {
-            console.log(response);
-          });    
-    }
+    //     axios({
+    //         method: "post",
+    //         url: BASE_URL,
+    //         data: bodyFormData,
+    //         headers: { "Content-Type": "multipart/form-data" },
+    //     })
+    //     // kan later weg
+    //       .then(function (response) {
+    //         console.log(response);
+    //       })
+    //       .catch(function (response) {
+    //         console.log(response);
+    //       });    
+    // }
 
     render() {
+        {console.log(this.state.emails)}
+
         return (
             <section className="email-card">
                 <section className="email-card__content">
                     <h1 className="email-card__content__header">Hey, Gebruiker</h1>
                     <SearchBar onSubmit={this.onSubmit} />
-                    <p className="email-card__content__text">Voorbeeld@mail.com</p>
-                    <p className="email-card__content__text">Voorbeeld@mail.com</p>
-                    <p className="email-card__content__text">Voorbeeld@mail.com</p>
+                    <FileUploadComponent />
+                    {console.log(this.state.emails)}
+                    <ul>
+                        { this.state.emails.map((item, id) => <li key={id}>tt</li>) }
+                    </ul>
+                                            
                 </section>               
             </section>
         );
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -18,9 +18,9 @@ class SearchBar extends React.Component {
     render() {
         return (
             <section className="searchbar">
-                <form onSubmit={this.onSubmit} className="searchbar__form">
-                    <label className="searchbar__form__label" htmlFor="email">Zoek email</label>
-                    <input onChange={this.onChange} className="searchbar__form__input" id="email" placeholder="Zoek hier een emailadres" type="text" value={this.state.emailInput} />
+                <form onSubmit={this.onSubmit} className="searchbar__form" method="POST">
+                    <label className="searchbar__form__label" htmlFor="email">Nieuw emailadres</label>
+                    <input onChange={this.onChange} className="searchbar__form__input" id="email" placeholder="Typ een nieuw emailadres" type="text" value={this.state.emailInput} />
                     <button className="searchbar__form__button" type="submit">Voeg email toe</button> 
                 </form>             
             </section>
@@ -29,3 +29,6 @@ class SearchBar extends React.Component {
 }
 
 export default SearchBar;
+
+{/* <label className="registerForm__label" htmlFor="email">Email { !this.state.emailError.valid ? this.state.emailError.message : ''}</label>
+<input className={!this.state.emailError.valid && this.state.emailError.touched ? 'registerForm__input registerForm__input--incorrect' : 'registerForm__input'} type="text" id="email" name="email" onChange={this.handleInputChange}/> */}
